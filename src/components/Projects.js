3fb4718982e254518a816103ab21993f2323a350
jarriola989 <jarri023@fiu.edu>
2020-02-23 23:35:08 -0500

    finished about me page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,9 @@ import React from "react";
 import wise from "../images/projects/wise-code.png";
 import geek from "../images/projects/geektext.png";
 import rhaz from "../images/projects/rhaz.png";
+import MiamiHacks from "../images/MiamiHacks.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink, faCode } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = props => (
   <div className="projects">
@@ -11,9 +14,11 @@ const Projects = props => (
           <img src={wise} alt="wise-code" />
         </div>
         <div className="back">
-          <div>Wise Code - Mobile</div>
-          <div>
-            My Contributions <br />
+          <div className="project-name">
+            <strong>Wise Code - Mobile</strong>
+          </div>
+          <div className="contributions">
+            My Contributions:
             <ul>
               <li>
                 Implemented and maintained Graphql API for data management app
@@ -26,7 +31,25 @@ const Projects = props => (
               </li>
             </ul>
           </div>
-          <div></div>
+          <div className="tech">
+            Technologies used:
+            <ul>
+              <li>Graphql</li>
+              <li>Prisma</li>
+              <li>Yoga</li>
+              <li>Redis</li>
+              <li>Kubernetes</li>
+              <li>Docker</li>
+            </ul>
+          </div>
+          <div>
+            <button>
+              <FontAwesomeIcon icon={faLink} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faCode} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -35,13 +58,38 @@ const Projects = props => (
         <div className="front">
           <img src={rhaz} alt="rhaz" />
         </div>
-        <div className="back">RHAZ</div>
+        <div className="back">
+          <div className="project-name">
+            <strong>RHAZ</strong>
+          </div>
+          <div className="contributions">
+            My Contributions:
+            <ul>
+              <li>Implemented logic for alien movement and re-generation.</li>
+            </ul>
+          </div>
+          <div className="tech">
+            Technologies used:
+            <ul>
+              <li>Unity</li>
+              <li>C#</li>
+            </ul>
+          </div>
+          <div>
+            <button>
+              <FontAwesomeIcon icon={faLink} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faCode} />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
     <div className="project3">
       <div className="flip">
         <div className="front">COMING SOON</div>
-        <div className="back">COMING SOON</div>
+        <div className="back coming-soon"></div>
       </div>
     </div>
     <div className="project4">
@@ -49,7 +97,15 @@ const Projects = props => (
         <div className="front">
           <img src={geek} alt="geek-text" />
         </div>
-        <div className="back">Geek Text</div>
+        <div className="back">
+          <div className="contribution-long">
+            <strong>Geek Text</strong>
+            My Contributions:
+            <ul>
+              <li>Implemented logic for alien movement and re-generation.</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
